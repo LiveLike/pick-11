4998ea5debad1ea0daec5835c0286a521e5c4402
Prem <prem89k@gmail.com>
2022-12-21 21:26:04 +0530

    Update for quick demo

--- scripts/left_pane.js
+++ scripts/left_pane.js
@@ -1,134 +1,159 @@
+players = [];
+widgetsMap = new Map();
 
-players = []
-widgetsMap = new Map()
-
-GK_LIMIT = 3
-MID_LIMIT = 8
-FWD_LIMIT = 7 
-DEF_LIMIT = 8 
-
+GK_LIMIT = 3;
+MID_LIMIT = 8;
+FWD_LIMIT = 7;
+DEF_LIMIT = 8;
 
 function initLiveLike() {
-    LiveLike.init({
-        clientId: 'mOBYul18quffrBDuq2IACKtVuLbUzXIPye5S3bq5',
-      }).then(() => {
-        document.querySelector("#nickname").innerHTML  = LiveLike.userProfile.nickname +"'s Team"
-        fetchWidgets()
-      });
+  LiveLike.init({
+    clientId: "mOBYul18quffrBDuq2IACKtVuLbUzXIPye5S3bq5",
+  }).then(() => {
+    document.querySelectorAll('.profile_nickname').forEach(e => e.innerHTML = LiveLike.userProfile.nickname + "'s Team");
+    fetchWidgets();
+  });
 }
 
 function fetchWidgets() {
-    LiveLike.getWidgets({
-        programId: data.programId,
-        status: "pending", //Valid status values are 'scheduled', 'pending', 'published'
-        widgetKinds: ["image-number-prediction"],
-        ordering: "recent"
-      }).then(res => {
-        widgetsArray = res.results
-        widgetsArray.forEach(widget => {
-              widget.options.forEach(option =>{
-                players.push({"name":option.description,"age":34, "tag":widget.question,"club":option.description,"widgetId":widget.id, "optionId":option.id,"image":option.image_url})
-              })
-            
-            widgetsMap.set(widget.question,[])
-          });
-
-          fetchPreviousSelection()
-          createList()
+  LiveLike.getWidgets({
+    programId: data.programId,
+    status: "pending", //Valid status values are 'scheduled', 'pending', 'published'
+    widgetKinds: ["image-number-prediction"],
+    ordering: "recent",
+  }).then((res) => {
+    widgetsArray = res.results;
+    widgetsArray.forEach((widget) => {
+      widget.options.forEach((option) => {
+        players.push({
+          name: option.description,
+          age: 34,
+          tag: widget.question,
+          club: option.description,
+          widgetId: widget.id,
+          optionId: option.id,
+          image: option.image_url,
+        });
       });
+
+      widgetsMap.set(widget.question, []);
+    });
+
+    fetchPreviousSelection();
+    createList();
+  });
 }
 
 function fetchPreviousSelection() {
-    widget_interaction_array = []
-    widgetsArray.forEach(widget => {
-        widget_interaction_array.push({"kind":"image-number-prediction", "id":widget.id,"tag":widget.question})
-    })
+  widget_interaction_array = [];
+  widgetsArray.forEach((widget) => {
+    widget_interaction_array.push({
+      kind: "image-number-prediction",
+      id: widget.id,
+      tag: widget.question,
+    });
+  });
 
-    LiveLike.getWidgetInteractions({programId:data.programId,widgets:widget_interaction_array}).then(interactions => {
-        imagePredInteractions = interactions['image-number-prediction']
-        if(imagePredInteractions) {
-            imagePredInteractions.forEach(interaction => {
-                interaction.votes.forEach(vote => {
-                    if(vote.number === 1) {
-                        players.forEach(player => {
-                            if(player.optionId === vote.option_id){ 
-                                processSelection(player.tag,vote.option_id,player)
-                                return
-                            }
-                        })
-                        
-                    }
-                })
-            })
-        }
-        
-    })
+  LiveLike.getWidgetInteractions({
+    programId: data.programId,
+    widgets: widget_interaction_array,
+  }).then((interactions) => {
+    imagePredInteractions = interactions["image-number-prediction"];
+    if (imagePredInteractions) {
+      imagePredInteractions.forEach((interaction) => {
+        interaction.votes.forEach((vote) => {
+          if (vote.number === 1) {
+            document.querySelector('#teamListMainBlock').style.display = "none"
+            document.querySelector('.team-list-show').style.display = "block"
+            players.forEach((player) => {
+              if (player.optionId === vote.option_id) {
+                processSelection(player.tag, vote.option_id, player);
+                return;
+              }
+            });
+          }
+        });
+      });
+    }
+  });
 }
 
 function createList() {
+  let container = document.querySelector(".left-pane-block");
+  players.forEach((player) => {
+    //console.log(player)
+    const entryRow = document.createElement("div");
+    entryRow.setAttribute("href", "#");
+    entryRow.setAttribute("class", "players-list");
 
-    let container = document.querySelector(".list-group")
-    players.forEach(player => {
-        const entryRow = document.createElement('div');
-        entryRow.setAttribute('href', '#');
-        entryRow.setAttribute('class', 'list-group-item flex-column align-items-start');
-        
-        entryRow.innerHTML = `
-        <div class="d-flex justify-content-between">
+    entryRow.innerHTML = `
 
-            <div class="flex-column align-items-start">
-                <div class="d-flex justify-content-start">
-                    <h5 class="mb-1">${player.name}</h5>
-                    <div class="d-flex justify-content-center align-items-center" style="background:rgba(120, 221, 58, 0.4); border-radius:4px; margin-left:8px; padding:2px">
-                        ${player.tag}
-                    </div>
-
+            <div class="players-info-box">
+                <div class="img-block">
+                <img src="${player.image}" />
                 </div>
-                <p class="mb-1">Age:${player.age} | Club: ${player.club}</p>
+                <div class="content-block">
+                    <div class="d-flex align-items-center justify-content-start">
+                        <h5>${player.name}</h5>
+                        <div class="badge text-bg-danger">
+                            ${player.tag}
+                        </div>
+
+                    </div>
+                    <p>Age:${player.age} | Club: ${player.club}</p>
+                </div>
             </div>
-            <button tag=${player.tag} player = ${escape(JSON.stringify(player))} optionId=${player.optionId} class="ml-auto left-pane-btn" style="background: transparent;border: none;" onclick="select(this)">
-                   <img></img>
+            <button tag=${player.tag} player = ${escape(
+      JSON.stringify(player)
+    )} optionId=${
+      player.optionId
+    } class="ml-auto left-pane-btn" style="background: transparent;border: none;" onclick="select(this)">
+                   <img  />
             </button>
-        </div>
+       
         `;
 
-        container.appendChild(entryRow)
-    })
+    container.appendChild(entryRow);
+  });
 }
 
 function select(optionElm) {
-    let tag = optionElm.getAttribute('tag')
-    let optionId = optionElm.getAttribute('optionId')
-    player_info = JSON.parse(unescape(optionElm.getAttribute('player')))
-    processSelection(tag, optionId, player_info)
+  let tag = optionElm.getAttribute("tag");
+  let optionId = optionElm.getAttribute("optionId");
+  player_info = JSON.parse(unescape(optionElm.getAttribute("player")));
+  processSelection(tag, optionId, player_info);
 }
 
-function processSelection(tag, optionId, player_info){
-    let selectedArr = widgetsMap.get(tag)
-    let optionIndex = selectedArr.indexOf(optionId)
+function processSelection(tag, optionId, player_info) {
+  let selectedArr = widgetsMap.get(tag);
+  let optionIndex = selectedArr.indexOf(optionId);
 
-    if( optionIndex != -1){
-        selectedArr.splice(optionIndex, 1)
-        widgetsMap.set(tag,selectedArr)
-        document.querySelector(`[optionid='${optionId}']`).removeAttribute('remove')
-        removeFromRightPane(tag, optionId)
+  if (optionIndex != -1) {
+    selectedArr.splice(optionIndex, 1);
+    widgetsMap.set(tag, selectedArr);
+    document
+      .querySelector(`[optionid='${optionId}']`)
+      .removeAttribute("remove");
+    removeFromRightPane(tag, optionId);
+  } else {
+    if (tag === "goalkeeper" && selectedArr.length >= GK_LIMIT) {
+      alert("You can have maximum of " + GK_LIMIT + " goalkeepers");
+    } else if (tag === "midfielder" && selectedArr.length >= MID_LIMIT) {
+      alert("You can have maximum of " + MID_LIMIT + " mid players");
+    } else if (tag === "FWD" && selectedArr.length >= FWD_LIMIT) {
+      alert("You can have maximum of " + FWD_LIMIT + " forward players");
+    } else if (tag === "defender" && selectedArr.length >= DEF_LIMIT) {
+      alert("You can have maximum of " + DEF_LIMIT + " defence players");
     } else {
-        if(tag === "GK" && selectedArr.length >= GK_LIMIT) {
-            alert("You can have maximum of "+GK_LIMIT+" goalkeepers")
-        } else if (tag === "MID" && selectedArr.length >= MID_LIMIT){
-            alert("You can have maximum of "+MID_LIMIT+" mid players")
-        } else if (tag === "FWD" && selectedArr.length >= FWD_LIMIT){
-            alert("You can have maximum of "+FWD_LIMIT+" forward players")
-        } else if (tag === "DEF" && selectedArr.length >= DEF_LIMIT){
-                alert("You can have maximum of "+DEF_LIMIT+" defence players")
-        } else{
-            document.querySelector(`[optionid='${optionId}']`).setAttribute('remove','')
-            selectedArr.push(optionId)
-            widgetsMap.set(tag,selectedArr)
-            addToRightPane(tag, optionId, player_info)
-        }
-        
+      document
+        .querySelector(`[optionid='${optionId}']`)
+        .setAttribute("remove", "");
+      selectedArr.push(optionId);
+      widgetsMap.set(tag, selectedArr);
+      addToRightPane(tag, optionId, player_info);
+      addToSubmitPage(tag, optionId, player_info);
     }
-
-    document.querySelector(`#${tag}_Counter`).innerHTML = selectedArr.length
+  }
+  console.log(tag)
+  document.querySelector(`#${tag}_Counter`).innerHTML = selectedArr.length;
 }
--- scripts/right_pane.js
+++ scripts/right_pane.js
@@ -1,79 +1,96 @@
-function removeFromRightPane(tag, optionId){
-    let elmToRemove = document.querySelector(`[option_id='${optionId}']`)
-    if (elmToRemove) {
-        elmToRemove.remove()
-    }
+function removeFromRightPane(tag, optionId) {
+  let elmToRemove = document.querySelector(`[option_id='${optionId}']`);
+  if (elmToRemove) {
+    elmToRemove.remove();
+  }
 }
 
-function remove(tag, optionId){
-    processSelection(tag, optionId, undefined)
+function remove(tag, optionId) {
+  processSelection(tag, optionId, undefined);
 }
 
-
-function addToRightPane(tag, optionId, player_data){
-   // player_data = JSON.parse(player_data)
-    let container = document.querySelector(`#${tag.toLowerCase()}_container`)
-    let div = document.createElement('div')
-    div.setAttribute('class','col')
-    div.setAttribute('option_id', optionId)
-    div.setAttribute('style',"max-width:254px")
-
-    div.innerHTML = 
-    `<div class="card mb-3" style="max-width: 240px;min-width: 240px;">
-        <div class="row g-0">
-            
-            <div class="col-md-8">
-                <div class="card-body">
-                <h5 class="card-title">${player_data.name}</h5>
-                <p class="card-text">${player_data.age}</p>
-                <p class="card-text"><small class="text-muted">${player_data.tag}</small></p>
-                </div>
-            </div>
-            <button optionId=${optionId} style="background: transparent;border: none; position:absolute; right:0; width:auto" onclick="remove(\'${player_data.tag}\',\'${optionId}\')">
-                   <img src ="./close.png" ></img>
-            </button>
-            <div class="col-md-4">
-                <img style="position: absolute;bottom: 0;right: 0; height:70%" src="${player_data.image}" class="img-fluid rounded-start" alt="...">
-            </div>
-        </div>  
-    </div>`
-    container.appendChild(div)
-
+function addToRightPane(tag, optionId, player_data) {
+  // player_data = JSON.parse(player_data)
+  let container = document.querySelector(`#${tag.toLowerCase()}_container`);
+  let div = document.createElement("div");
+  div.setAttribute("class", "player-card-items");
+  div.setAttribute("option_id", optionId);
+  div.innerHTML = `
+  <div class="player-card-info-header">
+    <div class="player-card-infobox">
+        <h5>${player_data.name}</h5>
+        <h6>Age : ${player_data.age}</h6>
+        <div class="badge text-bg-danger">${player_data.tag}</div>
+        
+    </div>
+    <img src ="images/close.png" optionId=${optionId} class="close-btn" onclick="remove(\'${player_data.tag}\',\'${optionId}\')" />
+    </div>
+    <img src="${player_data.image}" class="player-img" alt="${player_data.name}" />`;
+  container.appendChild(div);
 }
 
-
-submitVotes = function(){
-    widgetsArray.forEach(element => {
-        selectedOptions = widgetsMap.get(element.question)
-        const data = {
-        votes:
-          element.options &&
-          element.options instanceof Array &&
-          element.options.map((option) => {
-            voteValue = 0
-            if (selectedOptions.indexOf(option.id) != -1){
-                voteValue = 1
-            }
-            return {
-              option_id: option.id,
-              number: voteValue,
-            };
-          }),
-      }
-
-      postVote(element.vote_url, data)
-    });
+function addToSubmitPage(tag, optionId, player_data) {
+  let container = document.querySelector(`.teamlist-${tag.toLowerCase()}s`)
+  let li = document.createElement("li");
+  li.innerHTML = 
+  `
+    <div class="teamlist-set-list-item">
+      <div class="img-block">
+        <img src="${player_data.image}">
+      </div>
+      <div class="content-block">
+        <h4>${player_data.name}</h4>
+        <h6>${player_data.tag}</h6>
+      </div>
+    </div>
+  `
+  console.log(tag)
+  console.log(`teamlist-${tag.toLowerCase()}s`)
+  container.appendChild(li);
 }
+
+download = function () {
+  htmlToImage.toJpeg(document.querySelector(".team-list-show"), { quality: 0.95 })
+  .then(function (dataUrl) {
+    var link = document.createElement('a');
+    link.download = 'image.jpeg';
+    link.href = dataUrl;
+    link.click();
+  });
+}
+
+submitVotes = function () {
+  widgetsArray.forEach((element) => {
+    selectedOptions = widgetsMap.get(element.question);
+    const data = {
+      votes:
+        element.options &&
+        element.options instanceof Array &&
+        element.options.map((option) => {
+          voteValue = 0;
+          if (selectedOptions.indexOf(option.id) != -1) {
+            voteValue = 1;
+          }
+          return {
+            option_id: option.id,
+            number: voteValue,
+          };
+        }),
+    };
+
+    postVote(element.vote_url, data);
+  });
+};
 
 function postVote(vote_url, data) {
-    return axios({
-        method: "post",
-        url: vote_url,
-        headers: {
-            Authorization: 'Bearer '+LiveLike.userProfile.access_token
-        },
-        data: data
-    }).then((w) => {
-        console.log(w);
-    });
+  return axios({
+    method: "post",
+    url: vote_url,
+    headers: {
+      Authorization: "Bearer " + LiveLike.userProfile.access_token,
+    },
+    data: data,
+  }).then((w) => {
+    console.log(w);
+  });
 }
